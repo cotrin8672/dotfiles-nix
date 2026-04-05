dae6f00c146a2e54e3643396bcc8398c0428a887
cotrin8672 <gummy8672@gmail.com>
2026-04-05 12:31:47 +0000

    nvim: infer java package on buffer enter

--- nvim/lua/shared/java_kotlin_package.lua
+++ nvim/lua/shared/java_kotlin_package.lua
@@ -17,17 +17,28 @@ local function infer_package_name(path)
 end
 
 function M.setup()
-  vim.api.nvim_create_autocmd("BufNewFile", {
+  vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     group = group,
     pattern = { "*.java", "*.kt" },
     callback = function(event)
+      if vim.b[event.buf].java_kotlin_package_checked then
+        return
+      end
+      vim.b[event.buf].java_kotlin_package_checked = true
+
       local package_name = infer_package_name(event.match)
       if not package_name then
         return
       end
 
       local lines = vim.api.nvim_buf_get_lines(event.buf, 0, -1, false)
-      if #lines > 1 or lines[1] ~= "" then
+      if lines[1] and lines[1]:match("^package%s+") then
+        return
+      end
+
+      if vim.iter(lines):any(function(line)
+        return line:match("%S") ~= nil
+      end) then
         return
       end
 
